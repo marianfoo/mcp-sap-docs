ab3a606797319a7f6090b0bef0c2a6d900a8f56c
„marianfoo“ <13335743+marianfoo@users.noreply.github.com>
2025-08-14 13:38:43 +0200

    test: more tests

--- test/tools/sap_docs_search/search-sapui5-docs.js
+++ test/tools/sap_docs_search/search-sapui5-docs.js
@@ -11,6 +11,12 @@ export default [
     tool: 'sap_docs_search',
     query: 'UI.Chart #SpecificationWidthColumnChart',
     expectIncludes: ['/sapui5/06_SAP_Fiori_Elements/column-micro-chart-1a4ecb8']
+  },
+  {
+    name: 'SAPUI5 ExtensionAPI',
+    tool: 'sap_docs_search',
+    query: 'extensionAPI',
+    expectIncludes: ['/sapui5/06_SAP_Fiori_Elements/using-the-extensionapi-bd2994b']
   }
 ];
 
