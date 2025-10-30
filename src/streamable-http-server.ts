bc869d58b9fe80e7de05ec9d2e9353f8c7ac7f54
GitHub Action <action@github.com>
2025-10-30 08:07:51 +0000

    chore: bump version to 0.3.20 [skip ci]

--- src/streamable-http-server.ts
+++ src/streamable-http-server.ts
@@ -10,7 +10,7 @@ import { logger } from "./lib/logger.js";
 import { BaseServerHandler } from "./lib/BaseServerHandler.js";
 
 // Version will be updated by deployment script
-const VERSION = "0.3.19";
+const VERSION = "0.3.20";
 
 
 // Simple in-memory event store for resumability
