291c3f32c84b0162235ff2daf142e694992fa6dc
GitHub Action <action@github.com>
2026-04-15 11:26:47 +0000

    chore: bump version to 0.3.38 [skip ci]

--- src/streamable-http-server.ts
+++ src/streamable-http-server.ts
@@ -13,7 +13,7 @@ import { prefetchFeatureMatrix } from "./lib/softwareHeroes/abapFeatureMatrix.js
 import { loadEmbeddingModel } from "./lib/embeddingSearch.js";
 
 // Version will be updated by deployment script
-const VERSION = "0.3.37";
+const VERSION = "0.3.38";
 const variant = getVariantConfig();
 
 
